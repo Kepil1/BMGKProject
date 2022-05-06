42534bfcab1289433b21433e6489b0fc2fb9f33b
Mert Kepildek <mertkepildek@gmail.com>
2022-05-07 00:41:02 +0300

    recipe post ekleme metodu eklendi

--- routes/recipeRouter.js
+++ routes/recipeRouter.js
@@ -1,11 +1,23 @@
 const {
     Router
 } = require('express')
+const Recipe = require('../models/Recipe')
 
 const app = Router()
 
 app.get('/', async (req, res) => {
     res.render('recipes')
+})
+
+app.post('/add', async (req, res) => {
+    const {name,portion,chefid} = req.body
+
+    try {
+        await Recipe.create({name, portion, chefid})
+        res.json({ok: true, message: 'Tarifiniz eklendi'})
+    } catch (error) {
+        res.json({ok:false, message: 'Hata'})
+    }
 })
 
 module.exports = app
