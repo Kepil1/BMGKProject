bf93cf1914ae306b66c96e600e4d9b106bfd76df
Mert Kepildek <mertkepildek@gmail.com>
2022-05-07 05:04:27 +0300

    şef güncelleme

--- routes/chefRouter.js
+++ routes/chefRouter.js
@@ -53,4 +53,20 @@ app.delete('/delete/:id', async (req, res) => {
     }
 })
 
+app.put('/update/:id/:name', async (req, res) => {
+    const {id,name} = req.params
+    try {
+        if (name == null || name == '') return res.status(500).json({ok:false, message: 'Lütfen ad giriniz!'})
+        const data = await Chef.findById(id).lean()
+        if (!data) {
+            return res.status(500).json({ok: false, message: 'Böyle bir şef bulunamadı'})
+        }
+        data.name = name
+        await Chef.findByIdAndUpdate(id, data, {new:true})
+        res.status(200).json({ok: true, message: 'Şef güncellendi'})
+    } catch (error) {
+        res.status(500).json({ok: false, message: 'Şef güncellenirken hata oluştu'})
+    }
+})
+
 module.exports = app
