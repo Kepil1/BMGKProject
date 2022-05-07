2b377692cb7b58a78a81ee6fb054e822180a401b
Mert Kepildek <mertkepildek@gmail.com>
2022-05-07 05:18:33 +0300

    tarif güncelleme

--- routes/recipeRouter.js
+++ routes/recipeRouter.js
@@ -38,4 +38,42 @@ app.post('/add', async (req, res) => {
     }
 })
 
+app.delete('/delete/:id', async (req, res) => {
+    const {id} = req.params
+    try {
+        const data = await Recipe.findById(id).lean()
+        if (!data) {
+            return res.status(500).json({ok: false, message: 'Böyle bir tarif bulunamadı'})
+        }
+
+        await Recipe.findByIdAndDelete(id)
+        res.status(200).json({ok: true, message: 'Tarif silindi'})
+    } catch (error) {
+        res.status(500).json({ok: false, message: 'Tarif silinirken hata oluştu'})
+    }
+})
+
+app.put('/update/:id/:name/:portion/:chefname', async (req, res) => {
+    const {id,name,portion,chefname} = req.params
+    try {
+        if (name == null || name == '') return res.status(500).json({ok:false, message: 'Lütfen tarif adı giriniz!'})
+        if (portion == null || portion == '') return res.status(500).json({ok:false, message: 'Lütfen porsiyon giriniz!'})
+        if (chefname == null || chefname == '') return res.status(500).json({ok:false, message: 'Lütfen chefname giriniz!'})
+
+        const data = await Recipe.findById(id).lean()
+        if (!data) {
+            return res.status(500).json({ok: false, message: 'Böyle bir tarif bulunamadı'})
+        }
+        
+        data.name = name
+        data.portion = portion
+        data.chefname = chefname
+        
+        await Recipe.findByIdAndUpdate(id, data, {new:true})
+        res.status(200).json({ok: true, message: 'Tarif güncellendi'})
+    } catch (error) {
+        res.status(500).json({ok: false, message: 'Tarif güncellenirken hata oluştu'})
+    }
+})
+
 module.exports = app
